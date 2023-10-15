d2b9f2cf54fe769ab36366839ac509ff6572d89a
SteakTheStake <gabecorr25@gmail.com>
2023-10-15 12:52:48 -0700

    changes

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,9 +3,7 @@ export const Footer = () => {
     <footer className="bottom-div">
       <div className="middle">
         <div className="nobtn">
-          <p className="halfo">
-            "Minecraft" is a trademark of Mojang. This site is not affiliated
-            with Mojang or Microsoft.
+          <p className="halfo">"Minecraft" is a trademark of Mojang. This site is not affiliated with Mojang or Microsoft.
           </p>
           <p>
             ©&nbsp;<span id="year"></span>&nbsp;Summit. All rights reserved.
